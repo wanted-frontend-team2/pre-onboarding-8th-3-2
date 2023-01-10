2574b9d9ca5408d35b9f9fe61a88c3b7b35acdf0
Jooseulgi <iauuai21@gmail.com>
2023-01-11 01:18:32 +0900

    chore: tailwind 추가

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        bg: "#CAE9FF",
+        blue: "#007BE9",
+        gray: "999999",
+      },
+    },
+  },
+  plugins: [],
+};

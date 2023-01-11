121072f184ac78e0b8e6d9d2ddbf067375247963
황채림 <cofla159@naver.com>
2023-01-12 04:20:33 +0900

    feat: 키보드 이동 기능
    키보드의 위, 아래 키를 이용해 추천 검색어를 이동
    - ref 사용하여 DOM 탐색
    - 최상단/최하단 항목에서는 최하단/최상단으로 이동

--- src/components/RecommendedWords.tsx
+++ src/components/RecommendedWords.tsx
@@ -1,18 +1,31 @@
+/* eslint-disable no-console */
 import { SearchResultType } from "../types";
 import BoldMatchParts from "./BoldMatchParts";
 
 function RecommendedWords({
   results,
   inputValue,
+  listRef,
+  focusIndex,
+  focusRef,
 }: {
   results: SearchResultType[];
   inputValue: string;
+  listRef: any;
+  focusIndex: number;
+  focusRef: any;
 }): JSX.Element {
   return (
-    <ul>
+    <ul ref={listRef}>
       {results.length > 0 ? (
-        results.map(({ sickCd, sickNm }) => (
-          <li key={sickCd}>{BoldMatchParts(inputValue, sickNm)}</li>
+        results.map(({ sickCd, sickNm }, index) => (
+          <li
+            key={sickCd}
+            ref={index === focusIndex ? focusRef : undefined}
+            className={index === focusIndex ? "bg-slate-100	" : ""}
+          >
+            {BoldMatchParts(inputValue, sickNm)}
+          </li>
         ))
       ) : (
         <div>검색어 없음</div>
--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,11 +1,16 @@
+/* eslint-disable no-console */
 /* eslint-disable no-alert */
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import RecommendedWords from "./RecommendedWords";
+import { SearchResultType } from "../types";
 
 function SearchBox(): JSX.Element {
   const [inputValue, setInputValue] = useState<string>("");
-  const [searchResults, setSearchResults] = useState<[]>([]);
+  const [searchResults, setSearchResults] = useState<SearchResultType[]>([]);
+  const [focusIndex, setFocusIndex] = useState<number>(-1);
+  const focusRef = useRef<any>(null);
+  const listRef = useRef<any>(null);
 
   useEffect(() => {
     const getSearchResults = async () => {
@@ -14,7 +19,8 @@ function SearchBox(): JSX.Element {
           `${process.env.REACT_APP_API_ADDRESS}?q=${inputValue}`
         );
         setSearchResults(response.data);
-        console.info("calling api");
+        // console.info("calling api");
+        // console.log("inputValue에 따른 api 응답", inputValue, response.data);
       } catch (e) {
         if (e instanceof Error) {
           alert(`통신에 실패했습니다. 다시 시도해주세요: ${e.message}`);
@@ -24,14 +30,44 @@ function SearchBox(): JSX.Element {
     if (inputValue) getSearchResults();
   }, [inputValue]);
 
+  function onKeyUpHandler(e: any) {
+    if (searchResults.length === 0) {
+      return;
+    }
+    if (e.key === "ArrowDown") {
+      if (
+        listRef.current &&
+        listRef.current.childElementCount === focusIndex + 1
+      ) {
+        setFocusIndex(0);
+        return;
+      }
+      setFocusIndex((index) => index + 1);
+    }
+    if (e.key === "ArrowUp") {
+      if (focusIndex === -1) {
+        setFocusIndex(searchResults.length - 1);
+        return;
+      }
+      setFocusIndex((index) => index - 1);
+    }
+  }
+
   return (
     <>
       <input
         type="text"
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
+        onKeyUp={onKeyUpHandler}
       />
-      <RecommendedWords results={searchResults} inputValue={inputValue} />
+      <RecommendedWords
+        results={searchResults}
+        inputValue={inputValue}
+        listRef={listRef}
+        focusIndex={focusIndex}
+        focusRef={focusRef}
+      />
     </>
   );
 }

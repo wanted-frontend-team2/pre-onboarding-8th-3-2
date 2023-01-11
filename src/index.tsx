db595298eb4703549e5552b468a6afffb04f2521
황채림 <cofla159@naver.com>
2023-01-11 21:24:32 +0900

    refactor: App.tsx 삭제
    App.tsx를 삭제하고 index.tsx에서 바로 컴포넌트 렌더링 하도록 변경
    - 컴포넌트 구조가 단순하여 App.tsx가 하는 역할이 없다고 생각
    - 추후 여러 컴포넌트를 모아 App으로 랩핑할 필요가 생길 때 다시 생성

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import SearchBox from "./components/SearchBox";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <SearchBox />
   </React.StrictMode>
 );

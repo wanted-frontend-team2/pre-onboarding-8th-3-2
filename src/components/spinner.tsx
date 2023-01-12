59f1d6c210b675bc2df1d286c1f7923f9426d650
YeongMin Ahn <ahn0min95@gmail.com>
2023-01-13 04:23:18 +0900

    Fix: src/components/spinner.tsx 삭제

--- src/components/spinner.tsx
+++
@@ -1,9 +0,0 @@
-function Spinner() {
-  return (
-    <div className='relative w-24 h-24 animate-spin rounded-full bg-sky-400 '>
-      <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-20 h-20 bg-gray-200 rounded-full border-2 border-white' />
-    </div>
-  );
-}
-
-export default Spinner;

f720369c5d22c9b4eefa4bb6ff8c824bae9a9dd7
ian <onlordev@gmail.com>
2023-01-13 14:30:49 +0900

    feat: api 요청 내 cache 기능 추가

--- src/util/api.ts
+++ src/util/api.ts
@@ -1,19 +1,33 @@
 import axios from 'axios';
 
+const cache = new Map();
+
 async function getSearchResults(input: string) {
-  console.info('calling api');
   try {
+    if (cache.has(input)) {
+      console.info('return cache');
+
+      return cache.get(input);
+    }
+    console.info('calling api');
+
     const response = await axios.get(
       `${process.env.REACT_APP_API_ADDRESS}?q=${input}`,
     );
 
-    return { state: 'success', data: response.data };
+    const successResult = { state: 'success', data: response.data };
+
+    cache.set(input, successResult);
+
+    return successResult;
   } catch (e) {
     if (e instanceof Error) {
       alert(`통신에 실패했습니다. 다시 시도해주세요: ${e.message}`);
     }
 
-    return { state: 'fail', data: [] };
+    const failResult = { state: 'fail', data: [] };
+
+    return failResult;
   }
 }
 

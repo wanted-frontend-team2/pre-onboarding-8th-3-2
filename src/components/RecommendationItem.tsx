176b07f0ca5599ae33071b3909721e5760e2b8bd
YeongMin Ahn <ahn0min95@gmail.com>
2023-01-13 19:47:21 +0900

    Revert "Fix: "whitespace-pre" 를 추가하여 공백문제 해결"
    This reverts commit 2218e054c4d0e35078b7c5d0546ca64ad39dfc20.

--- src/components/RecommendationItem.tsx
+++ src/components/RecommendationItem.tsx
@@ -20,6 +20,7 @@ function RecommendationItem({
     const beforeName = sickName.slice(0, firstIndex);
     const restName = sickName.slice(firstIndex + targetValue.length);
 
+    console.log(firstIndex, 'first');
     return (
       <>
         {beforeName}
@@ -33,7 +34,7 @@ function RecommendationItem({
     <li
       role="presentation"
       onClick={() => setInputValue(sick)}
-      className={`whitespace-pre flex py-1.5 items-center cursor-pointer ${
+      className={`flex py-1.5 items-center cursor-pointer ${
         isSelected && 'bg-blue-200'
       }`}
     >

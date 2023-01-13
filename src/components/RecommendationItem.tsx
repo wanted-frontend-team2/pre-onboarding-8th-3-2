2218e054c4d0e35078b7c5d0546ca64ad39dfc20
YeongMin Ahn <ahn0min95@gmail.com>
2023-01-13 19:18:09 +0900

    Fix: "whitespace-pre" 를 추가하여 공백문제 해결

--- src/components/RecommendationItem.tsx
+++ src/components/RecommendationItem.tsx
@@ -20,7 +20,6 @@ function RecommendationItem({
     const beforeName = sickName.slice(0, firstIndex);
     const restName = sickName.slice(firstIndex + targetValue.length);
 
-    console.log(firstIndex, 'first');
     return (
       <>
         {beforeName}
@@ -34,7 +33,7 @@ function RecommendationItem({
     <li
       role="presentation"
       onClick={() => setInputValue(sick)}
-      className={`flex py-1.5 items-center cursor-pointer ${
+      className={`whitespace-pre flex py-1.5 items-center cursor-pointer ${
         isSelected && 'bg-blue-200'
       }`}
     >
